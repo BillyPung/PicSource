398b3e72ac6739b8ba1a3f2bae5291a6de342f5d
Billy Peng <62238461+BillyPung@users.noreply.github.com>
2022-12-07 18:52:05 +0400

    Add files via upload

---
+++ img/Ray.js
@@ -0,0 +1,40 @@
+class Ray {
+  constructor(x, y, startAngle) {
+    this.origin = x;
+    this.origin = y;
+    this.distance = 0;
+    this.angle = startAngle;
+    this.stillOnScreen = true;
+    this.speed = 1;
+  }
+  move() {
+    this.distance = this.distance + this.speed;
+    this.speed = this.speed + this.distance / 150;
+  }
+  check() {
+    this.stillOnScreen = (this.distance < p5.width / 2);
+  }
+  show() {
+    p5.push();// remember the fill and stroke before
+    p5.fill(255, 255, 255, 255 - this.distance );
+    p5.stroke(200, 200, 200, 255 - this.distance * 2);
+    p5.strokeWeight(5);
+    // if (this.stillOnScreen) {
+    //   for (var i = 0; i < 6; i++) {
+    //     var x = this.origin + (this.distance + i * 8) * cos(this.angle);
+    //     var y = this.origin + (this.distance + i * 8) * sin(this.angle);
+    //     ellipse(x, y, i *2, i*2);
+    //   }
+      if (this.stillOnScreen) {
+      for (i = 0; i < 3; i++) {
+        var x = this.origin + (this.distance + i *2) * p5.cos(this.angle);
+        var y = this.origin + (this.distance + i *2) * p5.sin(this.angle);
+        var xTo = this.origin + (this.distance + i * 10 ) * p5.cos(this.angle);
+        var yTo = this.origin + (this.distance + i * 10 ) * p5.sin(this.angle);
+        p5.line(x,y,xTo,yTo);
+      }
+      //line(this.origin, this.origin, this.origin + (this.distance +  20) *cos(this.angle),this.origin + (this.distance +  20) *sin(this.angle))
+    }
+    p5.pop();  //restore fill and stroke
+  }
+}
