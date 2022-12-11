ba4bba13f192e1012f845595e1e45e6e31c9020c
Billy Peng <62238461+BillyPung@users.noreply.github.com>
2022-12-11 18:13:43 +0400

    Create RayL.js

---
+++ img/RayL.js
@@ -0,0 +1,45 @@
+class RayL {
+  constructor(x, y, startAngle) {
+    this.originX = x;
+    this.originY = y;
+    this.distance = 0;
+    this.angle = startAngle;
+    this.stillOnScreen = true;
+    this.speed = 1;
+  }
+  move() {
+    this.distance = this.distance + this.speed;
+    this.speed = this.speed + this.distance / 400;
+
+  }
+  check() {
+    this.stillOnScreen = (this.distance < width / 2);
+  }
+  destroy() {
+    delete(this);
+  }
+  show() {
+    p5.push();// remember the fill and stroke before
+    p5.fill(255, 255, 255);//, 255 - this.distance );
+    p5.stroke(255, 255, 255);//, 255 - this.distance * 2);
+    p5.strokeWeight(2);
+    // if (this.stillOnScreen) {
+    //   for (var i = 0; i < 6; i++) {
+    //     var x = this.origin + (this.distance + i * 8) * cos(this.angle);
+    //     var y = this.origin + (this.distance + i * 8) * sin(this.angle);
+    //     ellipse(x, y, i *2, i*2);
+    //   }
+      if (this.stillOnScreen) {
+      for (var i = 0; i < 3; i++) {
+        var x = this.originX + (this.distance + i *2) * p5.cos(this.angle);
+        var y = this.originY + (this.distance + i *2) * p5.sin(this.angle);
+        var xTo = this.originX + (this.distance + i * 10 ) * p5.cos(this.angle);
+        var yTo = this.originY + (this.distance + i * 10 ) * p5.sin(this.angle);
+        p5.line(x,y,xTo,yTo);
+      }
+      //line(this.origin, this.origin, this.origin + (this.distance +  20) *cos(this.angle),this.origin + (this.distance +  20) *sin(this.angle))
+    }
+    else{this.destroy();}
+    p5.pop();  //restore fill and stroke
+  }
+}
